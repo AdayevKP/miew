16c8652de117ca0225d9636334002e06428ba3f8
adayevkp <adayevkp@gmail.com>
2019-06-05 18:04:15 +0400

    Fix bug, due to which automat always equals null.

--- src/utils/GraphUtils.js
+++ src/utils/GraphUtils.js
@@ -18,6 +18,7 @@ export default class GraphUtils {
     this._startNode = startNode;
     this._parents[this._startNode._index] = { node: this._startNode, parent: this._startNode._index };
     function doDfs(Node, automat) {
+      let resAut = automat;
       this._visited[Node._index] = true;
       const bonds = Node._bonds;
       for (let i = 0; i < Node._bonds.length; i++) {
@@ -26,15 +27,18 @@ export default class GraphUtils {
         if (!this._visited[nextNode._index] && autClone.eatNode(nextNode)) {
           this._distances[nextNode._index] = this._distances[Node._index] + 1;
           this._parents[nextNode._index] = { node: nextNode, parent: Node._index };
-          doDfs.call(this, nextNode, autClone);
+          const newAut = doDfs.call(this, nextNode, autClone);
+          resAut = newAut._resultWord > resAut._resultWord ? newAut : resAut;
         }
       }
+
+      return resAut;
     }
 
     this._distances = new Array(this._vertices.length).fill(-1);
     this._visited = new Array(this._vertices.length).fill(false);
     const automat = new FSMachine();
-    doDfs.call(this, startNode, automat);
+    this._automat = doDfs.call(this, startNode, automat);
     const maxIndx = this._distances.indexOf(Math.max(...this._distances));
     return this._vertices.find(V => V._index === maxIndx);
   }
